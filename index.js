0f67386243004589d7bfee4eaaf94695ba1b0755
stensis <irenenjuguna98@gmail.com>
2022-06-10 10:48:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
+
+// Write a function that
+//      takes an array as an argument of objects 
+//          *note: record is an array that contains 3 objects
+//      uses find() method to test each object to see if the result is "W"
+//      return the year when the win occurred
+//      return undefined if no winis found 
+
+// an already declared function as argument 
+function superbowlWin (array) {
+    function winFinder(element){
+        return element.result === "W"
+    }
+    const yearWin = array.find(winFinder)
+    if (yearWin){
+        return yearWin.year
+    } else {
+        return undefined
+    }
+}
+
